e262651c126c2bc782bab0532b62885c8a08f25f
Mario <jmferna91@gmail.com>
2019-08-22 11:22:05 -0400

    added comments and removed security keys etc...

--- runV1.py
+++ runV1.py
@@ -8,23 +8,23 @@ import subprocess
 import dropbox
 import os
 
-#access token: MHQU8ikz2LAAAAAAAAAAfW3NoCKAuBFUnPNAdRzrMEtuPYlVQc-9gEXi6xCyIRs7
+access_token = "token"
 
-# Get your app key and secret from the Dropbox developer website
-app_key = '3ef0ygak70uki01'
-app_secret = 'lqpa4iqas30m1ec'
+#key and secret from the Dropbox developer website
+app_key = 'key'
+app_secret = 'secret'
  
+#file name is the date and time it is recorded
 def getFileName():
     return datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
  
-
- 
+#function to upload video file ot dropbox
 def dropboxUpload(fileToUpload):
-
+    #path in dropbox where save file is located
     dropbox_path='/Apps/SecuritySystemAlpha'
-    dbx=dropbox.Dropbox("MHQU8ikz2LAAAAAAAAAAfW3NoCKAuBFUnPNAdRzrMEtuPYlVQc-9gEXi6xCyIRs7")
+    dbx=dropbox.Dropbox(access_token)
     with open(fileToUpload, 'rb') as f:
-        dbx.files_upload(f.read(),dropbox_path+fileToUpload,mute=True) # The change from f to f.read() was made to comply with the late-2016 change in the API.
+        dbx.files_upload(f.read(),dropbox_path+fileToUpload,mute=True) 
 
     #delete file from pi so not to take up too much space
     os.remove(fileToUpload)
@@ -33,25 +33,24 @@ def dropboxUpload(fileToUpload):
 sensorPin = 4
  
 GPIO.setmode(GPIO.BCM)
-#GPIO.setup(sensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(sensorPin, GPIO.IN)
  
 prevState = False
 currState = False
  
+#camera image is upside down, so this flips the image and also sets the resolution
 cam = picamera.PiCamera()
 cam.rotation = 180
 cam.resolution = (1024, 768)
 
 fileName = ""
  
+#Keep looping infinitely waiting to sense motion
 while True:
     time.sleep(0.1)
     prevState = currState
     currState = GPIO.input(sensorPin)
     if currState != prevState:
-        #newState = "HIGH" if currState else "LOW"
-        #print "GPIO pin %s is %s" % (sensorPin, newState)
         if currState:
             fileName = getFileName()
             print "Starting Recording..."
@@ -62,11 +61,6 @@ while True:
             cam.stop_preview()
             cam.stop_recording()
             print "Stopped Recording"
-            #print "Sending Mail Notification..."
-            #subprocess.call("mail -s 'Motion Detected' jmferna91@gmail.com < /home/pi/message.txt", shell=True)
-            #print "Complete"
             print "Uploading footage to Dropbox..."
-	    #testing
-	    #fileName = "./message1.txt"
             dropboxUpload(fileName)
             print "Complete"
